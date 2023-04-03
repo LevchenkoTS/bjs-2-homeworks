68b13c6c96b509c49e01e8e633da0bba574841f4
Tatiana Levchenko <medvedovskaya.tatiana@gmail.com>
2023-04-03 21:21:21 +0300

    hw4

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,40 @@
+"use strict"
+
 function Student(name, gender, age) {
-  
+    this.name = name;
+    this.gender = gender;
+    this.age = age;
+    this.marks = [];
 }
 
+    let student1 = new Student("Василиса", "женский", 19);
+    let student2 = new Student("Артём", "мужской", 25);
+
 Student.prototype.setSubject = function (subjectName) {
-  
+    this.subject = subjectName;  
 }
 
 Student.prototype.addMarks = function (...marks) {
-  
+    if (this.marks === undefined) {
+        return undefined;
+    } 
+    this.marks.push(...marks);
 }
 
 Student.prototype.getAverage = function () {
-  
+    if (this.marks === undefined || this.marks.length === 0) {
+        return 0;
+    } else {
+        const average = this.marks.reduce((acc, marc) => acc + marc, 0) / this.marks.length;
+        return average;
+    }
 }
 
+
 Student.prototype.exclude = function (reason) {
-  
+
+delete this.marks;
+delete this.subject;
+this.excluded = reason;
+
 }
